d0d15bff8f244538616b3828416eea02439fc6bd
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-01 16:22:34 +0700

    cors origin conflict

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(cors());
 
 app.use('/api', [...routes]);
