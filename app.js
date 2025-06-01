3764c92192fd4c40740f7e3dfbaacad242018d09
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-01 18:38:13 +0700

    cors origin conflict

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://mahitala-iota.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
