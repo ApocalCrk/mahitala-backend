79a5952ceaaa43b82d2cab103ec419564da80f82
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-01 19:02:07 +0700

    cors origin conflict

--- app.js
+++ app.js
@@ -6,19 +6,10 @@ const routes = require("./src/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: 'https://mahitala-iota.vercel.app',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(express.json());
+app.use(cors());
 
 app.use('/api', [...routes]);
 
