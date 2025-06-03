ff419061f42088b7d7d1f9c9d3552d883e927640
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-03 15:32:11 +0700

    fix: implement custom CORS policy to restrict access to allowed origins

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const routes = require("./src/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use('/api', [...routes]);
+app.use("/api", [...routes]);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
