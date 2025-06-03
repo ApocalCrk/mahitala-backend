4c2961ff9f678679c3519daf464027ff5918f4d7
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-03 14:35:02 +0700

    fix: add CORS headers to enhance API security

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(cors());
 
 app.use('/api', [...routes]);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mahitala-re.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
