8773dad5a711f3605888ecee34da3afe74df47bc
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-07 17:32:13 +0700

    fix: change forecast route from POST to GET and update parameter handling

--- src/routes/cuaca/route.js
+++ src/routes/cuaca/route.js
@@ -60,17 +60,17 @@ router.get('/cuaca/warning', async (req, res) => {
 //   }
 // });
 
-router.post('/crop/predict', async (req, res) => {
+router.get('/crop/predict', async (req, res) => {
     const response = getCropPredictions(req, res);
     return response;
 });
 
-router.post('/crop/recommendation', async(req, res) => {
+router.get('/crop/recommendation', async(req, res) => {
     const response = getCropRecommendation(req, res);
     return response;
 });
 
-router.post('/cuaca/weekly', async (req, res) => {
+router.get('/cuaca/weekly', async (req, res) => {
     const response = getForecastWeekly(req, res);
     return response;
 });
