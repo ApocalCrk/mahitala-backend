57d0620dfc8bb31d5a05d4d549d8f43658044933
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-22 09:57:32 +0700

    chore: feature for token verification optional user

--- src/middleware/verifyTokenOptional.js
+++ src/middleware/verifyTokenOptional.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyTokenOptional = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers['Authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
