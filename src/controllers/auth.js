4e09429d0486213cfc604668fc0cbc43e59e6867
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-07-03 13:35:27 +0700

    chore: update authentication controller for improved error handling and user feedback

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -9,6 +9,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const register = async (req, res) => {
   try {
     const { username, token } = req.body;
+    console.log(req.body);
 
     const existingUser = await AuthModel.isUsernameTaken(username);
     if (existingUser.length > 0) {
