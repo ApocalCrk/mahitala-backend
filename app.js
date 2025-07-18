db5d625f738b715a0a2f3ef0f81f1fc3dd39f9c6
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-07-18 18:41:43 +0700

    chore: change check komoditas price

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 
 app.use('/api', [...routes]);
 
