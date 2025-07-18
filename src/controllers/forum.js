e939512b46055a65be8627fa820e65d42a0b6900
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-07-18 19:19:30 +0700

    chore: change check komoditas price

--- src/controllers/forum.js
+++ src/controllers/forum.js
@@ -239,7 +239,7 @@ const deleteSecondReply = async (req, res) => {
   }
 };
 
-const checkHargaKomoditasProdusen = async (res) => {
+const checkHargaKomoditasProdusen = async (req, res) => {
     try {
         const result = await ForumModel.checkKomoditasHargaPasar();
         res.json(result);
